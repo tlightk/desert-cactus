511b3975589197eb3a3bb303725fe1abc26eedd6
Clay Xiong <klayxi@gmail.com>
2022-04-28 17:00:20 +0900

    created expensesReducer.js

---
+++ src/services/expenses.js
@@ -0,0 +1,12 @@
+export const GetExpenses = async (dispatch) => {
+    try {
+        // api call
+        const expenses = [
+            {id: 1, description: 'Groceries', amount: 3.23},
+            {id: 2, description: 'Gas', amount: 21.65},
+            {id: 3, description: 'Rent', amount: 150.00},
+        ];
+    } catch {
+        console.log('Error')
+    }
+}
