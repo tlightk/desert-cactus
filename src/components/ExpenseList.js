53145276674e7b634198af359855280fd7bfd0f7
Clay Xiong <klayxi@gmail.com>
2022-05-02 11:50:54 +0900

    created rows and columns displaying description and amount

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,15 +1,34 @@
-import { React, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { GetExpenses } from '../services/expenses';
+import { React, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GetExpenses } from "../services/expenses";
+import { Button, Row, Col } from "react-bootstrap";
 
 export default () => {
-    // hook used to get dispatch needed from services
-    const dispatch = useDispatch();
+  // hook used to get dispatch needed from services
+  const dispatch = useDispatch();
+  const expenses = useSelector((state) => state.expensesReducer.expenses);
 
-    useEffect(() => {
-        // empty array to call once
-        GetExpenses(dispatch);
-    }, []);
+  useEffect(() => {
+    // empty array to call once
+    GetExpenses(dispatch);
+  }, []);
 
-    return 'expense list';
-}
+  return expenses.map((e) => (
+    <div style={{ marginBottom: "1rem" }}>
+      <ListRow expense={e} />
+    </div>
+  ));
+};
+
+const ListRow = ({ expense }) => {
+  return (
+    <div>
+      <Row>
+        <Col>{expense.description}</Col>
+        <Col>{expense.amount}</Col>
+        <Col><Button style={{ float: "right" }} variant="warning">Edit</Button></Col>
+      </Row>
+      <hr />
+    </div>
+  );
+};
